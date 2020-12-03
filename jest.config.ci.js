9d22a3d08b6ae3bb230bb71c0e11ed76ba0b6620
ning <16nding@gmail.com>
2020-12-03 10:52:29 +0800

    feat: junit output config

--- jest.config.ci.js
+++ jest.config.ci.js
@@ -1,6 +1,9 @@
 const base = require('./jest.config')
 module.exports = Object.assign({}, base, {
-    reporters: ["jest-junit"],
+    reporters: ["default",[ 'jest-junit', {
+        outputDirectory: 'test-results/jest',
+        outputName: 'results.xml',
+    } ]],
     collectCoverage: true,
     collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],
     coverageDirectory: 'coverage',
