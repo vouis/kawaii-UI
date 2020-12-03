aac42cecc0450e66960a0ef2687e2fe4a9d6fadf
ning <16nding@gmail.com>
2020-12-03 16:34:36 +0800

    feat: user

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,10 +1,1 @@
-import * as React from 'react';
-import { render } from 'react-dom'
-import Icon from './icon'
-const fn :React.MouseEventHandler = (e) =>{
-    console.log(e.target)
-    // console.log((e.target as SVGUseElement).style)
-}
-render(
-    <Icon name='planet'  onMouseEnter={fn}/>,document.getElementById('root'),
-)
+export {default as Icon} from './icon';
