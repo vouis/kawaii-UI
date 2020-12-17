b908f5eafe379392b0518b704df69ce938d01d70
vouis <16nding@gmail.com>
2020-12-17 20:46:24 +0800

    feat: button style

--- lib/button/Button.tsx
+++ lib/button/Button.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import './button.scss'
+import getClasses from "../util/getClasses";
 interface Props extends React.ButtonHTMLAttributes<any>{
 
 }
 const Button:React.FunctionComponent<Props>=(props)=>{
     const {...restProps} = props;
-    return(<button {...restProps}></button>)
+    const classes = getClasses('kawaii-button');
+    return(<span {...restProps} className={classes('wrapper')}/>)
 }
 export default Button;
