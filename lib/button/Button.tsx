e6b53139a9033c68a3aeb4aef40d3f635530fe5e
vouis <16nding@gmail.com>
2021-01-04 20:54:45 +0800

    feat: 2077 button using after

--- lib/button/Button.tsx
+++ lib/button/Button.tsx
@@ -1,20 +1,18 @@
-import React ,{Fragment}from "react";
+import React from "react";
 import './button.scss'
 import getClasses from "../util/getClasses";
 interface Props extends React.ButtonHTMLAttributes<any>{
         theme?:string;
 }
 const Button:React.FunctionComponent<Props>=(props)=>{
-    const {className,theme,children,...restProps} = props;
+    const {className,theme,...restProps} = props;
     const classes = getClasses('kawaii-button',className);
     console.log(props);
     return(
-        <Fragment>
-        <span {...restProps} className={theme?classes(theme):classes('2077')}>
-            {children}
-        </span>
-            <span children={children} className={theme?classes(theme+'2077-glitch'):classes('2077-glitch')}/>
-            </Fragment>
+
+        <span {...restProps} className={theme?classes(theme):classes('2077')}/>
+
+
             )
 }
 export default Button;
